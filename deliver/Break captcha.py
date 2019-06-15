1eb2ef1ebe85d3e15028c32914097baf7f9f7396
Rafael Goncalves <rafaelgoncalves@riseup.net>
2019-06-15 12:34:48 -0300

    Create notebook.

---
+++ deliver/Break captcha.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import os
+import random
+import gc
+
+import torch
+import torchvision
+import torchvision.datasets as datasets
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import numpy as np
+import scipy.io
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[7]:
+
+
+data_dir = os.path.abspath(os.path.relpath('../data'))
+image_dir = os.path.abspath(os.path.relpath('../doc/images'))
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+
+
+
+# In[4]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
